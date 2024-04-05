1c0f6075801cd52b1545d7025821f8ff4b2645af
root <fachrizalfahmy@gmail.com>
2024-04-05 02:02:29 +0000

    update detail update IP

--- index.js
+++ index.js
@@ -11,56 +11,18 @@ app.use(express.static('public'));
 app.use(express.json());
 
 app.post('/update-ip', (req, res) => {
-    const { vmName, newIP } = req.body;
-    const vmIP = '10.0.1.254'; // Replace with actual VM IP
+    const { vmName, osName, newIP } = req.body;
 
-    // SSH commands to update IP address
-    const updateCommands = [
-        `hostnamectl set-hostname ${vmName}`,
-        `sudo sed -i 's/${vmIP}/${newIP}/' /etc/netplan/50-cloud-init.yaml`,
-        `sudo netplan apply`
-    ];
+    const scriptPath = './script/update.sh'; // Update with the actual path
 
-    const conn = new sshClient();
-    conn.on('ready', () => {
-        console.log('SSH Connection established');
-        executeCommand(0);
-    }).connect({
-        host: vmIP,
-        username: 'root',
-        privateKey: require('fs').readFileSync('./.ssh/id_rsa')
-    });
-
-    const executeCommand = (index) => {
-        if (index < updateCommands.length) {
-            conn.exec(updateCommands[index], (err, stream) => {
-                if (err) {
-                    console.error(`Error executing command: ${err.message}`);
-                    return res.status(500).send('Internal Server Error');
-                }
-                stream.on('close', (code, signal) => {
-                    console.log(`Command ${updateCommands[index]} exited with code ${code}`);
-                    executeCommand(index + 1);
-                }).on('data', (data) => {
-                    console.log(`STDOUT: ${data}`);
-                }).stderr.on('data', (data) => {
-                    console.error(`STDERR: ${data}`);
-                    return res.status(400).send('Bad Request');
-                });
-            });
-        } else {
-            console.log('All commands executed successfully');
-            // Reboot the VM
-            conn.exec('sudo reboot', (err, stream) => {
-                if (err) {
-                    console.error(`Error executing reboot command: ${err.message}`);
-                    return res.status(500).send('Internal Server Error');
-                }
-                console.log('Reboot command executed successfully');
-                res.send('IP Address Updated and VM Rebooting');
-            });
+    exec(`${scriptPath} ${osName} ${vmName} ${newIP}`, { cwd: '/tmp' }, (error, stdout, stderr) => {
+        if (error || stderr) {
+            console.error(`Error: ${error ? error.message : stderr}`);
+            return res.status(500).send('Error updating IP address');
         }
-    };
+        console.log(`Commands executed successfully: ${stdout}`);
+        res.send('IP Address Updated and VM Rebooting');
+    });
 });
 
 app.post('/resize-disk', (req, res) => {
---
+++ script/update.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ "${1}" == "ubuntu" ]; then
+    # Fetch Ubuntu type
+    export TYPE=$(ssh 10.0.1.254 hostnamectl | grep ubuntu | awk '{print $3}')
+
+    # Update IP based on Ubuntu type
+    if [ "$TYPE" = "ubuntu-lite" ] || [ "$TYPE" = "ubuntu-kvm" ]; then
+        ssh 10.0.1.254 "hostnamectl set-hostname '${2}'"
+        ssh 10.0.1.254 "sudo sed -i 's/10.0.1.254/${3}/' /etc/netplan/50-cloud-init.yaml"
+        ssh 10.0.1.254 "reboot"
+    else
+        ssh 10.0.1.254 "hostnamectl set-hostname '${2}'"
+        ssh 10.0.1.254 "sudo sed -i 's/10.0.1.254/${3}/' /etc/netplan/00-installer-config.yaml"
+        ssh 10.0.1.254 "reboot"
+    fi
+else
+    ssh -l root 10.0.1.254 "hostnamectl set-hostname '${2}'"
+    ssh -l root 10.0.1.254 "sed -i 's/IPADDR=10.0.1.254/IPADDR=${3}/' /etc/sysconfig/network-scripts/ifcfg-eth0"
+    ssh -l root 10.0.1.254 "reboot"
+fi
